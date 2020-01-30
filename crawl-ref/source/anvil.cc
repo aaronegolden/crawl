658dbbbcdbe499df562a596275e72e3e043de46a
hellmonk <nldehaan91@gmail.com>
2020-01-29 21:55:06 -0500

    Anvils part 3. Allow anvils to change weapon subtype. Not really happy with the control structure here, lots of ugly breakpoints and probably can be gamed pretty easily, but it should at least function. Allows the anvil to try to change weapon type to a "better" weapon in the same category, respecting player size, skill, and handedness. If trying to improve base type fails, it'll do something else instead.

--- crawl-ref/source/anvil.cc
+++ crawl-ref/source/anvil.cc
@@ -20,6 +20,190 @@
 #include "terrain.h"
 #include "view.h"
 
+//return whether the base type of the weapon sucks ass or not
+static bool _basetype_sucks(item_def &item)
+{
+    switch(item.base_type)
+    {
+        case OBJ_WEAPONS:
+        {
+            return item.sub_type == WPN_WHIP || item.sub_type == WPN_MACE
+                || item.sub_type == WPN_FLAIL || item.sub_type == WPN_RAPIER
+                || item.sub_type == WPN_LONG_SWORD
+                || item.sub_type == WPN_WAR_AXE || item.sub_type == WPN_HAND_AXE
+                || item.sub_type == WPN_SPEAR || item.sub_type == WPN_TRIDENT
+                || item.sub_type == WPN_HALBERD || item.sub_type == WPN_SHORTBOW
+                || item.sub_type == WPN_DAGGER;
+        }
+        default:
+            return false;
+    }
+}
+
+/*try to improve the weapon's base type, taking into account handedness
+and the plyer's skill level. Return true if successful and false otherwise*/
+static bool _try_improve_weapon_basetype(item_def &item)
+{
+    //don't even try
+    if (item.base_type != OBJ_WEAPONS)
+        return false;
+    
+    int wpn_skill = you.skill(item_attack_skill(item), 10, true, false);
+    int shield_skill = you.skill(SK_SHIELDS, 10, true, false);
+    bool formicid = you.species == SP_FORMICID;
+    size_type size = you.body_size(PSIZE_TORSO, true);
+    
+    
+    switch(item.sub_type)
+    {
+        case WPN_WHIP:
+        case WPN_MACE:
+            item.sub_type = WPN_FLAIL;
+            return true;
+        case WPN_RAPIER:
+            item.sub_type = WPN_LONG_SWORD;
+            return true;
+        case WPN_LONG_SWORD:
+            item.sub_type = WPN_SCIMITAR;
+            return true;
+        case WPN_SHORTBOW:
+            item.sub_type = coinflip() ? WPN_ARBALEST : WPN_LONGBOW;
+            return true;
+        case WPN_HAND_AXE:
+            item.sub_type = WPN_WAR_AXE;
+            return true;
+        case WPN_SPEAR:
+            item.sub_type = WPN_TRIDENT;
+            return true;
+        case WPN_HALBERD:
+            item.sub_type = WPN_GLAIVE;
+            return true;
+        case WPN_WAR_AXE:
+        {
+            if(x_chance_in_y(100 - shield_skill, 60) || formicid)
+            {
+                if(size >= SIZE_MEDIUM)
+                {
+                    item.sub_type = WPN_BATTLEAXE;
+                    return true;
+                }
+                else
+                {
+                    item.sub_type = WPN_BROAD_AXE;
+                    return true;
+                }
+            }
+            else if (size >= SIZE_MEDIUM)
+            {
+                item.sub_type = WPN_BROAD_AXE;
+                return true;
+            }
+            break;
+        }
+        case WPN_SCIMITAR:
+        {
+            if(size >= SIZE_MEDIUM || x_chance_in_y(100 - shield_skill, 60))
+            {
+                item.sub_type = WPN_DOUBLE_SWORD;
+                return true;
+            }
+            else if (coinflip() || size < SIZE_MEDIUM)
+            {
+                item.sub_type = WPN_DEMON_BLADE;
+                return true;
+            }
+            break;
+        }
+        case WPN_DOUBLE_SWORD:
+        {
+            if(size >= SIZE_MEDIUM && (x_chance_in_y(100 - shield_skill, 60) || formicid)
+                && (wpn_skill > 200 || wpn_skill < 100))
+            {
+                item.sub_type = WPN_TRIPLE_SWORD;
+                return true;
+            }
+            else
+            {
+                item.sub_type = WPN_DEMON_BLADE;
+                return true;
+            }
+            break;
+        }
+        case WPN_QUARTERSTAFF:
+        {
+            if (size >= SIZE_MEDIUM)
+            {
+                item.sub_type = WPN_GREAT_MACE;
+                return true;
+            }
+            else
+            {
+                item.sub_type = coinflip() ? WPN_MORNINGSTAR : WPN_DEMON_WHIP;
+                return true;
+            }
+            break;
+        }
+        case WPN_TRIDENT:
+        {
+            if (one_chance_in(3) || size < SIZE_MEDIUM)
+            {
+                item.sub_type = WPN_DEMON_TRIDENT;
+                return true;
+            }
+            else if (x_chance_in_y(100 - shield_skill, 60) || formicid)
+            {
+                item.sub_type = WPN_HALBERD;
+                return true;
+            }
+            break;
+        }
+        case WPN_FLAIL:
+        {
+            if ((x_chance_in_y(100 - shield_skill, 60)) || formicid)
+            {
+                item.sub_type = WPN_QUARTERSTAFF;
+                return true;
+            }
+            else if (one_chance_in(3))
+            {
+                item.sub_type = coinflip() ? WPN_MORNINGSTAR : WPN_DEMON_WHIP;
+                return true;
+            }
+            break;
+        }
+        case WPN_BATTLEAXE:
+        {
+            if (wpn_skill > 200 || wpn_skill < 100)
+            {
+                item.sub_type = WPN_EXECUTIONERS_AXE;
+                return true;
+            }
+            break;
+        }
+        case WPN_GLAIVE:
+        {
+            if (wpn_skill > 200 || wpn_skill < 100)
+            {
+                item.sub_type = WPN_BARDICHE;
+                return true;
+            }
+            break;
+        }
+        case WPN_ARBALEST:
+        case WPN_LONGBOW:
+        {
+            if (wpn_skill > 200 || wpn_skill < 100)
+            {
+                item.sub_type = WPN_TRIPLE_CROSSBOW;
+                return true;
+            }
+            break;
+        }
+        default:
+            return false;
+    }
+    return false;
+}
 
 //returns whether the anvil successfully modified an item.
 static bool _perform_forge_action(item_def &item)
@@ -28,17 +212,26 @@ static bool _perform_forge_action(item_def &item)
     {
         case OBJ_WEAPONS:
         {
+            //chance to turn the item into an artifact, or add additional artifact properties
             if(one_chance_in(is_artefact(item) ? 3 : 5))
             {
                 anvil_modify_artp(item);
                 return true;
             }
+            //chance to improve base type; continue if the base type can't be improved
+            if(one_chance_in(_basetype_sucks(item) ? 3: 5))
+            {
+                if(_try_improve_weapon_basetype(item))
+                    return true;
+            }
+            //chance to rebrand weapon, or guaranteed rebrand if we can't raise enchant
             brand_type brand = get_weapon_brand(item);
             if (item.plus >= MAX_WPN_ENCHANT || one_chance_in(brand == SPWPN_NORMAL ? 3 : 6))
             {
                 rebrand_weapon(item);
                 return true;
             }
+            //if we didn't do anything else, increase the weapon's enchantment
             else
             {
                 item.plus = min(item.plus + 1 + random2(3), MAX_WPN_ENCHANT);
@@ -48,11 +241,13 @@ static bool _perform_forge_action(item_def &item)
         case OBJ_ARMOUR:
         {
             int max_ench = armour_max_enchant(item);
+            //chance to turn the item into an artifact or add artifact properties
             if (item.plus >= max_ench || one_chance_in(is_artefact(item) ? 3 : 5))
             {
                 anvil_modify_artp(item);
                 return true;
             }
+            //if we didn't do that, raise enchantment
             else
             {
                 item.plus = min(item.plus + 1 + random2(3), max_ench);
