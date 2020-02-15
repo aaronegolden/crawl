0561387c99dae7d95f2f358d860d46fe2f06c52e
hellmonk <nldehaan91@gmail.com>
2020-02-14 22:21:11 -0500

    Tengu reaver book rework. Merge the books into one book with fireball/corrosive bolt/battlesphere.

--- crawl-ref/source/mon-util.cc
+++ crawl-ref/source/mon-util.cc
@@ -2546,10 +2546,6 @@ static vector<mon_spellbook_type> _mons_spellbook_list(monster_type mon_type)
         return { MST_TENGU_CONJURER_I, MST_TENGU_CONJURER_II,
                  MST_TENGU_CONJURER_III, MST_TENGU_CONJURER_IV };
 
-    case MONS_TENGU_REAVER:
-        return { MST_TENGU_REAVER_I, MST_TENGU_REAVER_II,
-                 MST_TENGU_REAVER_III };
-
     case MONS_DEEP_ELF_MAGE:
         return { MST_DEEP_ELF_MAGE_I, MST_DEEP_ELF_MAGE_II,
                  MST_DEEP_ELF_MAGE_III, MST_DEEP_ELF_MAGE_IV,
