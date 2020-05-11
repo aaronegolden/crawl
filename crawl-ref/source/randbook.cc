1f0b1ebfca48fb03f4912e7b4f27f9c0e66a24da
hellmonk <nldehaan91@gmail.com>
2020-05-11 13:51:15 -0400

    Rework kiku book gifts. In lieu of a more serious kiku overhaul.

--- crawl-ref/source/randbook.cc
+++ crawl-ref/source/randbook.cc
@@ -980,27 +980,14 @@ void make_book_kiku_gift(item_def &book, bool first)
     {
         chosen_spells[0] = SPELL_ESSENCE_SPRAY;
         chosen_spells[1] = SPELL_VAMPIRE_KISS;
+        chosen_spells[2] = SPELL_ANIMATE_DEAD;
     }
     else
     {
-        chosen_spells[0] = coinflip() ? SPELL_ANIMATE_DEAD
-            : SPELL_CIGOTUVIS_EMBRACE;
-        chosen_spells[1] = coinflip()
-            ? SPELL_AGONY : SPELL_EXCRUCIATING_WOUNDS;
-        chosen_spells[2] = SPELL_DEATH_CHANNEL;
-        spell_type extra_spell;
-        do
-        {
-            extra_spell = random_choose(SPELL_ANIMATE_DEAD,
-                                        SPELL_CIGOTUVIS_EMBRACE,
-                                        SPELL_AGONY,
-                                        SPELL_EXCRUCIATING_WOUNDS);
-            for (int i = 0; i < 3; i++)
-                if (extra_spell == chosen_spells[i])
-                    extra_spell = SPELL_NO_SPELL;
-        }
-        while (extra_spell == SPELL_NO_SPELL);
-        chosen_spells[3] = extra_spell;
+        chosen_spells[0] = SPELL_EXCRUCIATING_WOUNDS;
+        chosen_spells[1] = SPELL_AFFLICTION;
+        chosen_spells[2] = coinflip() ? SPELL_DEATH_CHANNEL :
+                SPELL_CIGOTUVIS_EMBRACE;
     }
 
     sort(chosen_spells, chosen_spells + RANDBOOK_SIZE, _compare_spells);
