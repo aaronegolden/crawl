689d716ea771fdc0829d8aefcae94ae38b89c11f
Aaron Golden <aegolden@gmail.com>
2020-05-22 00:15:27 -0700

    Get game running again after bad merge plus handling gauntlets.

--- crawl-ref/source/dat/dlua/gauntlet.lua
+++ crawl-ref/source/dat/dlua/gauntlet.lua
@@ -299,7 +299,7 @@ tier1_gauntlet_arenas = {
   {
     first  = {mons = "spiny frog simulacrum / wyvern simulacrum " ..
                       "/ hornet simulacrum", min = 1, max = 1},
-    second = {mons = "simulacrum place:D:12", min = 2, max = 4},
+    second = {mons = "simulacrum place:D:3", min = 2, max = 4},
     liquid = "water",
     plant  = "withered",
   },
@@ -456,7 +456,7 @@ tier2_gauntlet_arenas = {
   {
     first  = {mons = "hydra simulacrum / harpy simulacrum " ..
                       "/ ice dragon simulacrum", min = 1, max = 1},
-    second = {mons = "simulacrum place:D:15", min = 2, max = 4},
+    second = {mons = "simulacrum place:D:3", min = 2, max = 4},
     liquid = "water",
     plant  = "withered",
   },
@@ -564,7 +564,7 @@ tier2_gauntlet_arenas = {
   },
   {
     first  = {mons = "death drake", min = 1, max = 1},
-    second = {mons = "zombie place:D:15 / spectre place:D:15", min = 2,
+    second = {mons = "zombie place:D:3 / spectre place:D:3", min = 2,
               max = 4},
     liquid = "water",
     plant  = "withered"
